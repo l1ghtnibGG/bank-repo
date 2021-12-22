c7887ad3fca74e327d02462a5f70d820e864fc26
vlad kryglikov <vladkryglikov@mail.ru>
2021-12-22 04:21:32 +0300

    Fix

--- Innowise/TRIGGER-8.sql
+++ Innowise/TRIGGER-8.sql
@@ -1,13 +1,25 @@
-CREATE TRIGGER verification ON Банки
+USE [Innowise]
+GO
+/****** Object:  Trigger [dbo].[verification]    Script Date: 22.12.2021 04:08:53 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER TRIGGER [dbo].[verification] ON [dbo].[Banks]
 FOR INSERT, UPDATE
 AS 
 DECLARE @bal_kart INT
 DECLARE @bal_bank INT
-SELECT @bal_kart = SUM(Карточки.Баланс)
-FROM Клиенты JOIN Карточки ON Карточки.idКлиента = Клиенты.id
-GROUP BY Клиенты.Фамилия, Клиенты.Имя
-SELECT @bal_bank = Банки.[Баланс аккаунта]
-FROM Банки
+
+
+SELECT @bal_kart = SUM(Cards.BalanceOfCard)
+FROM Clients JOIN Cards ON Cards.ClientId = Clients.Id
+GROUP BY Clients.LastName, Clients.FirstName
+
+SELECT @bal_bank = Banks.Balance
+FROM Banks
+
+
 IF @bal_bank < @bal_kart
 BEGIN 
 ROLLBACK TRAN
